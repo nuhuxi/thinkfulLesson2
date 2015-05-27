c424a2d9418b009ab230615af6312e9e02412945
nuhuxi <mark.keckeis@gmail.com>
2015-05-27 13:39:36 -0700

    Done with Lesson 2

--- server.js
+++ server.js
@@ -45,36 +45,77 @@ app.post('/items', jsonParser, function(req, res) {
   }
 
   var item = storage.add(req.body.name);
+  console.log("Added this item: " + req.body.name);
   res.status(201).json(item);
 
 });
 
 app.delete('/items/:item', jsonParser, function(req, res) {
-
   if (!req.body) {
     return res.sendStatus(400);
   }
-  var findTheItem = function(deleteThis){
-    var itemToDelete = '';
+  var findTheItem = function(findThisItem){
+    var thisItemFound = '';
 
     for(var i = 0; i < storage.items.length; i++){
 
-      if(storage.items[i].id == deleteThis){
-        itemToDelete = storage.items[i].name;
-        return itemToDelete;
+      if(storage.items[i].id == findThisItem){
+        thisItemFound = storage.items[i];
+        return thisItemFound;
       };
 
     };
   };
-  
   var itemToDelete = req.params.item;
-  var nameOfItem = '';
-  nameOfItem = findTheItem(itemToDelete);
-  console.log("The name of the item to delete is: " + nameOfItem);
+  var deleteThisObject = '';
+  deleteThisObject = findTheItem(itemToDelete);
+  if(!deleteThisObject){
+    itemToDelete.name = "Sorry the item you have asked to delete isn't on the list";
+    itemToDelete.id = itemToDelete;
+  };
+  res.status(201).json(deleteThisObject);
+});
 
+app.put('/items/:item', jsonParser, function(req, res) {
 
+  if (!req.body) {
+    return res.sendStatus(400);
+  };
+
+  var findTheItem = function(findThisItem){
+    var thisIndexFound;
+
+    for(var i = 0; i < storage.items.length; i++){
+      if(storage.items[i].id == findThisItem.id){
+        thisIndexFound = i;
+        return thisIndexFound;
+      };
+    };
+
+    thisIndexFound = 999999;
+    return thisIndexFound;
+  };
+
+  var thisItemSent = req.body;
+
+  var editThisIndex = 999999;
+
+  editThisIndex = findTheItem(thisItemSent);
+
+  console.log("Change the name of this item: " + storage.items[editThisIndex].name);
+
+  if(editThisIndex < 999998){
+    storage.items[editThisIndex].name = req.body.name;
+    res.status(201).json(storage.items[editThisIndex]);
+
+  } else {
+    numberOfItemsInTheList = storage.items.length;
+    storage.items[numberOfItemsInTheList + 1] = req.body;
+    res.status(201).json(req.body);
+  };
 
 });
+
 /* The following code allows us to use the process.env object and the PORT property of that object */
 
 app.listen(process.env.PORT || 3000);
